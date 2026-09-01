7ef13807d530a07ec0e2626bd69d8ad73b544905
Stazkun01 <chakoubi2007@gmail.com>
2026-09-01 16:19:48 +0200

    updating files

--- variables/challenge6.js
+++ variables/challenge6.js
@@ -1,11 +1,12 @@
 import readline from 'readline/promises'
 import {stdin as input , stdout as output} from 'process'
 const rl = readline.createInterface({input,output})
-let score1 = parseFloat(await rl.question("quelle est votre note du premier partie?"));
-let score2 = parseFloat(await rl.question("quelle est votre note du deuxieme partie?"));
-let score3 = parseFloat(await rl.question("quelle est votre note du troisieme partie?"));
-let score4 = parseFloat(await rl.question("quelle est votre note du quatrieme partie?"));
-let score_total = (score1+score2)+(score3+score4);
+let score1 = await rl.question("quelle est votre note du premier partie?");
+let score2 = await rl.question("quelle est votre note du deuxieme partie?");
+let score3 = await rl.question("quelle est votre note du troisieme partie?");
+let score4 = await rl.question("quelle est votre note du quatrieme partie?");
+let score_total = +score1 + +score2 + +score3 + +score4;
 console.log(`Score total : ${score_total}`);
 let score_finale = (score_total/4);
 console.log(`Moyenne : ${score_finale}`);
+rl.close();
--- variables/challenge7.js
+++ variables/challenge7.js
@@ -1,8 +1,9 @@
 import readline from 'readline/promises'
 import { stdin as input, stdout as output } from 'process'
 const rl = readline.createInterface({input,output})
-let note_cc = parseFloat(await rl.question("quelle est votre note du controle continue ? "));
-let note_project = parseFloat(await rl.question("quelle est votre note du Projet ? "));
-let note_examenFinal = parseFloat(await rl.question("quelle est votre note du examen final ? "));
-let note_Final = ((note_cc * 2 + note_project * 3 + note_examenFinal * 5) / 10);
+let note_cc = await rl.question("quelle est votre note du controle continue ? ");
+let note_project = await rl.question("quelle est votre note du Projet ? ");
+let note_examenFinal = await rl.question("quelle est votre note du examen final ? ");
+let note_Final = ((+note_cc * 2 + +note_project * 3 + +note_examenFinal * 5) / 10);
 console.log(`Contrôle continu : ${note_cc} \nProjet : ${note_project}\nExamen : ${note_examenFinal}\n\nNote finale : ${note_Final}`);
+rl.close();
