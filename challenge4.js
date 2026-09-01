fa0b356adab51abb5e55bcae7142d3f4a1d6838d
Stazkun01 <chakoubi2007@gmail.com>
2026-09-01 10:43:42 +0200

    calcule de consommation

---
+++ challenge4.js
@@ -0,0 +1,7 @@
+let litres = 35;
+let kilometres = 500;
+let consomation = (litres/kilometres) * 100;
+console.log(`Distance : ${kilometres}\nCarburant : ${litres}\n\nConsommation : ${consomation}/100km`);
+//the code outputs a float value but i was Prohibited to use any built in functions like .toFixed() 
+// if you have a better solution fork the repo 
+//till then its staying like this
