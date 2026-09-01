826ba1389ccdb991e2d04f05cd2220b2e3989f81
Stazkun01 <chakoubi2007@gmail.com>
2026-09-01 10:21:22 +0200

    printing of data out of variables

---
+++ challenge1.js
@@ -0,0 +1,7 @@
+let Pseudo = "ShadowX";
+let niveau = 12;
+let age = 21;
+let score = 4580;
+let pays = "maroc";
+console.log(`Pseudo : ${Pseudo} \nNiveau : ${niveau} \nage : ${age} \nscore : ${score} \npays : ${pays}`);
+
