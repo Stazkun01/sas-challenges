99a39acbed61e01ab2eab23607018da470fa377b
sb3tach <dibt.chakibb@gmail.com>
2026-09-01 19:16:07 +0100

    update file

--- variables/challenge4.js
+++ variables/challenge4.js
@@ -1,7 +1,5 @@
 let litres = 35;
 let kilometres = 500;
 let consomation = (litres/kilometres) * 100;
-console.log(`Distance : ${kilometres}\nCarburant : ${litres}\n\nConsommation : ${consomation}/100km`);
-//the code outputs a float value but i was Prohibited to use any built in functions like .toFixed() 
-// if you have a better solution fork the repo 
-//till then its staying like this
+let consofix = Math.round(consomation * 100)/100;
+console.log(`Distance : ${kilometres}\nCarburant : ${litres}\n\nConsommation : ${consofix}/100km`);
