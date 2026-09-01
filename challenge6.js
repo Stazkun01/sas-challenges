56e327983d97c448a97085dc0192d8f3cee2aa69
Stazkun01 <chakoubi2007@gmail.com>
2026-09-01 11:29:58 +0200

    Calcul du score d'un examen

---
+++ challenge6.js
@@ -0,0 +1,11 @@
+import readline from 'readline/promises'
+import {stdin as input , stdout as output} from 'process'
+const rl = readline.createInterface({input,output})
+let score1 = parseInt(await rl.question("quelle est votre note du premier partie?"));
+let score2 = parseInt(await rl.question("quelle est votre note du deuxieme partie?"));
+let score3 = parseInt(await rl.question("quelle est votre note du troisieme partie?"));
+let score4 = parseInt(await rl.question("quelle est votre note du quatrieme partie?"));
+let score_total = (score1+score2)+(score3+score4);
+console.log(`Score total : ${score_total}`);
+let score_finale = (score_total/4);
+console.log(`Moyenne : ${score_finale}`);
