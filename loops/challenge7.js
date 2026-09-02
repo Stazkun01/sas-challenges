d4e21a83921b5d51a67b040e4e2c11b900047b46
Stazkun01 <chakoubi2007@gmail.com>
2026-09-02 15:21:39 +0200

    trouver le nombre superieur

---
+++ loops/challenge7.js
@@ -0,0 +1,10 @@
+let prompt = require('prompt-sync')();
+let N = +prompt("entrer le nombre du depart : ");
+let multip = N*N;
+console.log(`Resultat : ${N}`);
+while (N*N < 100){
+    N++;
+}
+console.log(multip);
+
+console.log(`${N} x ${N} = ${multip}`);
