89b0be22c9c70fa33403586f5fc5d90f84cf7b11
Stazkun01 <chakoubi2007@gmail.com>
2026-09-07 11:14:22 +0200

    supprimer doublons

--- arrays/challenge7.js
+++ arrays/challenge7.js
@@ -1,3 +1,16 @@
-function filtrerTableau(tab){
-    
+function supprimerDoublons(tab){
+    let arr = [];
+    for(let i = 0 ; i < tab.length ; i++){
+        let alrseen = false;
+        for (let j = 0 ; j < arr.length ; j++){
+            if (arr[j] == tab[i]){
+                 alrseen = true;
+            }
+        }
+        if(!alrseen){
+            arr[arr.length] = tab[i];
+        }
+    }
+    return arr;
 }
+console.log(supprimerDoublons([1,2,2,3,4,4,1]))
